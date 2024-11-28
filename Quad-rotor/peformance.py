4df4fe40e42d69abcc1575e3ba5fb0cdc562ef88
jangogol <gogol.pl@icloud.com>
2024-11-28 10:45:02 +0100

    updated results import from rotor sizing

--- Quad-rotor/peformance.py
+++ Quad-rotor/peformance.py
@@ -15,7 +15,7 @@ class PerformanceAnalysis:
 
         # Perform rotor sizing
         self.results = rotor_analysis.perform_analysis(c_t_o_fl=0.1, c_t_o_turn=0.2, c_t_o_turb=0.3)
-        self.rotor_radius, self.rotor_diameter, *_ = self.results
+        self.rotor_radius, self.rotor_diameter, self.v_tip, self.mach_number, self.omega, self.adv_ratio_fl, self.o_fl, self.o_turn, self.o_turb, self.solidity, self.aspect_ratios, self.n_z  = self.results
 
         # Derived parameters
         self.rotor_area = np.pi * (self.rotor_radius**2)  # Rotor swept area (m^2)
@@ -39,8 +39,10 @@ class PerformanceAnalysis:
         P_req_hover = ((self.mtow*self.g)**(3/2))/(np.sqrt(2*self.rho*self.calculate_disk_area())*self.FM)
         return P_req_hover
     
-    #def calculate_power_loading_hover(self):
-        #P_load_hover = 
+    def calculate_power_loading_hover(self):
+        P_load_hover = (self.mtow) / (self.calculate_power_req_hover()/1000)
+        return P_load_hover
+    
     
     def display_results(self):
         """Display power requirements and induced velocities."""
@@ -48,11 +50,13 @@ class PerformanceAnalysis:
         v_i_climb = self.calculate_climb_induced_velocity()
         disk_area = self.calculate_disk_area()
         power_requirements = self.calculate_power_req_hover()
+        power_loading_hover = self.calculate_power_loading_hover()
 
-        print(f'Hover induced velocity: {v_i_hover:.2f} m/s')
-        print(f'Climb induced velocity: {v_i_climb:.2f} m/s')
+        #print(f'Hover induced velocity: {v_i_hover:.2f} m/s')
+        #print(f'Climb induced velocity: {v_i_climb:.2f} m/s')
         print(f'Disk Area: {disk_area:.2f} m2')
         print(f'Power required for hover: {power_requirements / 1000:.2f} kW')
+        print(f'Power loading for hover: {power_loading_hover:.2f} kg/kW')
 
 
 # Main Execution
