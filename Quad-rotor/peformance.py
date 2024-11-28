88c00010e5ff3e907405f64f18969dc194f54296
fbwichers <fbwichers@gmail.com>
2024-11-28 11:28:12 +0100

    added profile drag power

--- Quad-rotor/peformance.py
+++ Quad-rotor/peformance.py
@@ -12,6 +12,7 @@ class PerformanceAnalysis:
         self.N_rotors = rotor_analysis.N_rotors
         self.rho = rotor_analysis.rho
         self.g = rotor_analysis.g
+        self.Cl_alpha = rotor_analysis.C_lalpha
 
         # Perform rotor sizing
         self.results = rotor_analysis.perform_analysis(c_t_o_fl=0.1, c_t_o_turn=0.2, c_t_o_turb=0.3)
@@ -43,6 +44,41 @@ class PerformanceAnalysis:
         P_load_hover = (self.mtow) / (self.calculate_power_req_hover()/1000)
         return P_load_hover
     
+    #In order to find Forward Flight P_req, we need to calculate some new values:
+    #Thrust coefficient
+    def calculate_C_T(self): 
+        self.T = self.mtow
+        C_T = self.T / (self.rho * np.pi * self.rotor_radius ** 2 *(self.omega * self.rotor_radius)**2 )
+        return C_T
+    
+    #Average C_l
+    def calculate_average_C_l(self):
+        average_C_l = 6.6 * self.calculate_C_T() / self.solidity
+        return average_C_l
+
+    #alpha_m
+    def calculate_alpha_m(self):
+        alpha_m = self.calculate_average_C_l() / self.Cl_alpha
+        return alpha_m
+    
+    #average_C_D_p
+    def calculate_average_C_D_p(self):
+        average_C_D_p = 0.0087 - 0.0216 * self.calculate_alpha_m() + 0.4 * self.calculate_alpha_m() ** 2
+        return average_C_D_p
+
+    #Profile drag power for hover
+    def calculate_P_p_hov(self):
+        P_p_hov = self.solidity * self.calculate_average_C_D_p() / 8 * self.rho * (self.omega * self.rotor_radius) ** 3 * np.pi * self.rotor_radius **2
+        return P_p_hov 
+
+    #Profile drag power for forward flight   
+    def calculate_P_p(self):
+        P_p = self.calculate_P_p_hov() * (1 + 4.65 * self.adv_ratio_fl ** 2)
+        return P_p
+
+
+      
+    
     
     def display_results(self):
         """Display power requirements and induced velocities."""
@@ -51,13 +87,14 @@ class PerformanceAnalysis:
         disk_area = self.calculate_disk_area()
         power_requirements = self.calculate_power_req_hover()
         power_loading_hover = self.calculate_power_loading_hover()
+        profile_power_fl = self.calculate_P_p()       
 
         #print(f'Hover induced velocity: {v_i_hover:.2f} m/s')
         #print(f'Climb induced velocity: {v_i_climb:.2f} m/s')
         print(f'Disk Area: {disk_area:.2f} m2')
         print(f'Power required for hover: {power_requirements / 1000:.2f} kW')
         print(f'Power loading for hover: {power_loading_hover:.2f} kg/kW')
-
+        print (f"profile drag power for fl:, {profile_power_fl / 1000: .2f} kW")
 
 # Main Execution
 if __name__ == "__main__":
