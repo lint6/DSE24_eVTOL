d444d75d8c04f7078d538127a7c6fc569abaaf70
lintongxuxu <xulintong3@gmail.com>
2024-12-01 22:07:18 +0100

    -Cleaned up hover related codes to unify climbing and descent -Added a graph for power and vertical rate relation -Commented equations for V_c/v_h < -2 since it is a very extreme flight case

--- Compound/ducted_fan_calc.py
+++ Compound/ducted_fan_calc.py
@@ -2,12 +2,13 @@ import numpy as np
 from math import *
 import scipy 
 import matplotlib.pyplot as plt 
+import sys
 
 
 #def calc_p_idd_kappa(self):
 #    self.calc_power_ideal_hover()
 #    self.calc_kappa_d()
-#    self.p_idd_kappa = self.p_idh * self.kappa_d
+#    self.p_idd_kappa = self.P_id_h * self.kappa_d
 #    return self.p_idd_kappa
 
 
@@ -23,18 +24,23 @@ def find_roots(coefficients):
 
 
 class Ducted_Fan_1: #vertical flight
-    def __init__(self, mass, radius=0.625, T_W_R= 1 , V_c=None, density=1.225, P_a = 58360): 
+    def __init__(self, mass, radius=0.625, TWR= 1 , V_c=None, density=1.225, g0 = 9.80665, P_a = None): 
+        # if V_c== None and P_a == None:
+        #     raise Exception('Vertical rate and power are both none, need at least one')
         # Required Inputs
         self.mass = mass  # Maximum takeoff weight
         self.radius = radius  # Fan radius (m)
+        self.g0 = g0
 
-        # Optional Inputs with Defaults
-        self.T_W_R = T_W_R  # Thrust-to-weight ratio
+        # Optional Inputs with Default
+        self.TWR = TWR  # Thrust-to-weight ratio
         self.V_c = V_c  # Climb freestream velocity (m/s)
         self.density = density  # Air density (kg/m^3)
         self.P_a = P_a # Power available set by user
 
         # Calculated Attributes (initialized as None)
+        self.V_c_nd = None
+        self.weight = None
         self.disc_loading = None
         self.thrust_loading = None 
         self.v_h = None  # Hover induced velocity
@@ -44,12 +50,11 @@ class Ducted_Fan_1: #vertical flight
         self.v_d_nd = None #above non dimensional
         self.v_c = None #induced velocity climb
         self.v_c_nd = None #above non dimensional
-        self.kappa_c = None # #ratio of ideal power abaliable o ideal power required in hover, from climb rate
-        self.p_idc = None # power ideal climb
-        self.p_idh = None  # power ideal hover
-        self.kappa_p = None  # #ratio of ideal power abaliable o ideal power required in hover, from power avalibale 
-        self.V_c_kappa = None # Vertical climb rate from kappa 
-        self.p_idd = 34000 # power ideal decent
+        self.kappa = None # #ratio of ideal power abaliable o ideal power required in hover, from climb rate
+        self.P_id_c = None # power ideal climb
+        self.P_id_h = None  # power ideal hover
+        # self.kappa = None  # #ratio of ideal power abaliable o ideal power required in hover, from power avalibale 
+        self.p_idd = None # power ideal decent
         self.kappa_d = None  # #ratio of ideal power abaliable o ideal power required in hover, from power avalable
         self.p_idd_kappa = None # power ideal descent from, kappa 
         self.V_d_kappa_d = None  #vertical Descnet rate from kappa RATE
@@ -57,124 +62,116 @@ class Ducted_Fan_1: #vertical flight
 
         
 
-    def calc_mass(self, new_mass):
+    def calc_mass(self):
+        new_mass = -sys.maxsize
         if new_mass > self.mass:
             self.mass = new_mass
-        return self.mass
+        self.weight = self.mass * self.g0
+        return self.mass, self.weight
     
-    #disc loading here is ACTUALLY thurst to area loading, T_W_R is thrust to wright ratio for example 1 is necessary for hovering but not enough to climb
+    #disc loading here is ACTUALLY thurst to area loading, TWR is thrust to wright ratio for example 1 is necessary for hovering but not enough to climb
     def calc_disc_loading(self):
-        self.disc_loading = (self.mass * self.T_W_R) / (np.pi * self.radius**2 )
-        return self.disc_loading
+        self.calc_mass()
+        self.disc_loading = self.mass / (np.pi * self.radius**2 ) #Definition of disc loading
+        self.thrust_loading = self.weight * self.TWR / (np.pi * self.radius**2 ) #Definition of thrust area loading
+        return self.disc_loading, self.thrust_loading
 
-    def calc_thrust_loading(self):
+    # def calc_thrust_loading(self):
+    #     self.calc_disc_loading()
+    #     self.thrust_loading = self.disc_loading * self.TWR * 9.81
+    #     return self.thrust_loading
+
+    def calc_v_h(self): #Induced velocity during hover
         self.calc_disc_loading()
-        self.thrust_loading = self.disc_loading * 9.81
-        return self.thrust_loading
-
-    # Induved velocity in book == v_h 
-    def calc_hover_induced_velovity(self):
-        self.calc_thrust_loading()
-        self.hover_induced_velocity = np.sqrt(self.thrust_loading/(2 * self.density))
-        self.v_h = self.hover_induced_velocity
+        self.v_h = np.sqrt(self.thrust_loading/(2 * self.density)) #eq 2.13
         return self.v_h
 
-    def calc_thrust_hover(self):
-        self.calc_hover_induced_velovity()
-        self.thrust = (2 * (np.pi * self.radius**2 )) * self.density * (self.v_h)**2
+    def calc_thrust_hover(self): #Thrust required for hover
+        self.calc_v_h()
+        self.thrust = 2 * (np.pi * self.radius**2 ) * self.density * (self.v_h)**2 #eq 2.12
         return self.thrust
     
-    # v_d is qquivalnet of v_c but direction is opposite 
-    # V_d, V_c is a freestream velocity decent n climb
-    # v_i induced velocity (non-hover)
-    # has dimension
-    # If we are decending fast, V_D/v_d >> 2 , fast_Decent 
-    def calc_v_d (self):
-        self.calc_disc_loading()
-        self.v_d= - 0.5 *self.V_c_kappa - np.sqrt(0.25 * self.V_c_kappa**2 - self.v_h **2)
-        return self.v_d
-    
-    def non_dimensionalize_v_d(self):
-        self.calc_v_d()
-        self.v_d_nd = (self.v_d/self.v_h)
-        return self.v_d_nd
-    
-    """
-    From here we are doing the other side of the graph 
-    """
-    def calc_v_c (self):
-        self.calc_disc_loading()
-        self.calc_V_c_kappa()
-        self.v_c = - 0.5 * self.V_c_kappa + np.sqrt(0.25 * self.V_c_kappa**2 + self.v_h **2)
-        return self.v_c
-    
-    def non_dimensionalize_v_c(self):
-        self.calc_v_c()
-        self.v_c_nd = (self.v_c/self.v_h)
-        return self.v_c_nd
-    
-    def calc_kappa_c(self): #ratio of ideal power abaliable o ideal power required in hover
-        self.calc_disc_loading()
-        self.calc_V_c_kappa()
-        kappa_v_c = 0.5 * self.V_c_kappa + np.sqrt(0.25 * self.V_c_kappa**2 + self.v_h **2)
-        self.kappa_c = kappa_v_c / self.v_h
-        return self.kappa_c
+    def calc_power_ideal_hover(self): #Power require for hover
+        self.calc_mass()
+        self.calc_v_h()
+        self.P_id_h = self.weight * self.v_h
+        return self.P_id_h
 
-    def calc_power_ideal_steady_climb(self):
-       self.calc_kappa_c()
-       self.p_idc = (self.mass * 9.81) * self.kappa_c * self.v_h
-       return self.p_idc
+    def calc_v_c (self): #Induced velocity during axial maneuver 
+        self.calc_v_h()
+        if self.V_c == None:
+            self.calc_V_c_kappa()
+        else:
+            self.calc_V_c_nd()
+        self.v_c_nd = -0.5 * self.V_c_nd + np.sqrt(0.25 * self.V_c_nd**2 + 1) #eq 2.14a
+        self.v_c = self.v_c_nd * self.v_h
+        return self.v_c, self.v_c_nd
     
-    def calc_power_ideal_hover(self):
-        self.calc_thrust_loading()
-        self.p_idh = (self.mass * 9.81) * 1 * self.v_h
-        return self.p_idh   
-    
-    def calc_kappa_p(self):
+    def calc_kappa_p(self): #kappa from known power, unknown climb rate
         self.calc_power_ideal_hover()
-        self.kappa_p = self.P_a / self.p_idh
-        return self.kappa_p
+        self.kappa = self.P_a / self.P_id_h
+        return self.kappa
     
-    def calc_V_c_kappa(self):
+    def calc_kappa_Vc(self): #kappa from known climb rate, unknown power
+        self.calc_power_ideal_hover()
+        self.calc_V_c_nd()
+        self.kappa = 0.5 * self.V_c_nd + sqrt(0.25 * self.V_c_nd ** 2 + 1)
+        return self.kappa
+    
+    def calc_V_c_kappa(self): #Axial rate of climb given kappa
         self.calc_kappa_p()
-        V_c_kappa_nd = self.kappa_p - (1/self.kappa_p)
-        self.V_c_kappa = V_c_kappa_nd * self.v_h
-        return self.V_c_kappa
+        self.calc_v_h()
+        self.V_c_nd = self.kappa - (1/self.kappa)
+        self.V_c = self.V_c_nd * self.v_h
+        return self.V_c, self.V_c_nd
+    
+    def calc_V_c_nd(self): #Find V_c_nd if calc_V_c_kappa() is not used
+        self.calc_v_h()
+        self.V_c_nd = self.V_c / self.v_h
+        return self.V_c_nd
+    
+    def calc_P_kappa(self): #Axial power required given kappa
+        self.calc_kappa_Vc()
+        self.calc_v_h()
+        self.calc_mass()
+        self.P_id_c = self.weight * self.v_h * self.kappa
+        return self.P_id_c
 
-
-    def calc_kappa_d(self):
-        self.calc_V_c_kappa()
-        self.calc_v_d()
-        if np.abs(self.V_c_kappa/self.v_d)>=2 :
-            self.kappa_d = 0.5 * (self.V_c_kappa) + np.sqrt(0.25 * self.V_c_kappa**2 - 1)
-        elif np.abs(self.V_c_kappa/self.v_d)<1 :
-            self.kappa_d = 0.5 * (self.V_c_kappa) + np.sqrt(0.25 * self.V_c_kappa**2 + 1)
-        return self.kappa_d
-
-
-    def calc_p_idd(self):
-        self.calc_V_c_kappa()
-        self.calc_v_d()
-        if np.abs(self.V_c_kappa/self.v_d)>=2 :
-            self.p_idd = - self.mass * 9.81 * (self.v_d + self.V_c_kappa)
-        elif np.abs(self.V_c_kappa/self.v_d)<1 :
-            self.p_idd = self.mass * 9.81 * (self.v_d + self.V_c_kappa)
-        return self.p_idd
-
-    def calc_V_d_kappa_d(self):
-        self.calc_V_c_kappa()
-        self.calc_v_d()
-        if np.abs(self.V_c_kappa/self.v_d)>=2 :
-            V_d_kappa_nd = - (self.kappa_d + (1/self.kappa_d))
-            self.V_d_kappa_d = V_d_kappa_nd * self.v_h  
-        elif np.abs(self.V_c_kappa/self.v_d)<1 :
-            V_d_kappa_nd = - self.kappa_d + (1/self.kappa_d)
-            self.V_d_kappa_d = V_d_kappa_nd * self.v_h
-        return self.V_d_kappa_d
+    '''
+    Commented due to Vd/vh>2 is unlikely to encounter during normal operation
+    # def calc_kappa_d(self):
+    #     self.calc_V_c_kappa()
+    #     self.calc_v_d()
+    #     if np.abs(self.V_c_kappa/self.v_d)>=2 :
+    #         self.kappa_d = 0.5 * (self.V_c_kappa) + np.sqrt(0.25 * self.V_c_kappa**2 - 1)
+    #     elif np.abs(self.V_c_kappa/self.v_d)<1 :
+    #         self.kappa_d = 0.5 * (self.V_c_kappa) + np.sqrt(0.25 * self.V_c_kappa**2 + 1)
+    #     return self.kappa_d
+    #
+    # def calc_p_idd(self):
+    #     self.calc_V_c_kappa()
+    #     self.calc_v_d()
+    #     if np.abs(self.V_c_kappa/self.v_d)>=2 :
+    #         self.p_idd = - self.mass * 9.81 * (self.v_d + self.V_c_kappa)
+    #     elif np.abs(self.V_c_kappa/self.v_d)<1 :
+    #         self.p_idd = self.mass * 9.81 * (self.v_d + self.V_c_kappa)
+    #     return self.p_idd
+    #
+    # def calc_V_d_kappa_d(self):
+    #     self.calc_V_c_kappa()
+    #     self.calc_v_d()
+    #     if np.abs(self.V_c_kappa/self.v_d)>=2 :
+    #         V_d_kappa_nd = - (self.kappa_d + (1/self.kappa_d))
+    #         self.V_d_kappa_d = V_d_kappa_nd * self.v_h  
+    #     elif np.abs(self.V_c_kappa/self.v_d)<1 :
+    #         V_d_kappa_nd = - self.kappa_d + (1/self.kappa_d)
+    #         self.V_d_kappa_d = V_d_kappa_nd * self.v_h
+    #     return self.V_d_kappa_d
+    '''
 
     
 class Ducted_Fan_2: #pure horizontal
-    def __init__(self, mass, Cd0, V, radius=0.625, T_W_R= 1 , gamma = 0, V_c=None, density=1.225, k_v_f = 1, related_fan = None): 
+    def __init__(self, mass, Cd0, V, radius=0.625, TWR= 1 , gamma = 0, V_c=None, density=1.225, k_v_f = 1, related_fan = None): 
         # Required Inputs
         self.mass = mass  # Maximum takeoff weight
         self.radius = radius  # Fan radius (m)
@@ -184,7 +181,7 @@ class Ducted_Fan_2: #pure horizontal
         self.gamma = gamma
 
         # Optional Inputs with Defaults
-        self.T_W_R = T_W_R  # Thrust-to-weight ratio
+        self.TWR = TWR  # Thrust-to-weight ratio
         self.V_c = V_c  # Climb freestream velocity (m/s)
         self.density = density  # Air density (kg/m^3)
         
@@ -262,7 +259,7 @@ class Ducted_Fan_2: #pure horizontal
 
 
 class Ducted_Fan_3: #angled climb
-    def __init__(self, mass, gamma, radius=0.625, T_W_R= 1 , V_c=None, V =4, density=1.225, D_h0 = 500, k_v_f = 1, P_a =45000,  related_fan1 = None, related_fan2 = None): 
+    def __init__(self, mass, gamma, radius=0.625, TWR= 1 , V_c=None, V =4, density=1.225, D_h0 = 500, k_v_f = 1, P_a =45000,  related_fan1 = None, related_fan2 = None): 
         # Required Inputs
         self.mass = mass  # Maximum takeoff weight
         self.radius = radius  # Fan radius (m)
@@ -273,7 +270,7 @@ class Ducted_Fan_3: #angled climb
         self.P_a = P_a # power avilable
 
         # Optional Inputs with Defaults
-        self.T_W_R = T_W_R  # Thrust-to-weight ratio
+        self.TWR = TWR  # Thrust-to-weight ratio
         self.V_c = V_c  # Climb freestream velocity (m/s)
         self.density = density  # Air density (kg/m^3)
         
--- Compound/ducted_fan_momentum_UI.py
+++ Compound/ducted_fan_momentum_UI.py
@@ -7,38 +7,37 @@ def func_min_locator(list1, list2):
     min_list2_index = list(list2).index(min_list2)
     return list1[min_list2_index], min_list2
 
-fan_1 = ducted_fan_calc.Ducted_Fan_1(mass=float(718/4))
+fan_1 = ducted_fan_calc.Ducted_Fan_1(mass=float(718/4), P_a=45000)
 
 # Calculate hover induced velocity
-print(f"Hover Induced Velocity: {fan_1.calc_hover_induced_velovity()}")
+print(f"Hover Induced Velocity: {fan_1.calc_v_h():.2f}")
 
 # Calculate thrust for hover
-print(f"Thrust for Hover: {fan_1.calc_thrust_hover()}")
+print(f"Thrust for Hover: {fan_1.calc_thrust_hover():.2f}")
 
 # Calculate power for steady climb
-print(f"Power for Steady Climb: {fan_1.calc_power_ideal_steady_climb()}")
+if fan_1.P_a == None: #give climb rate, not power
+    print(f"Given axial Rate of Climb: {fan_1.V_c:.2f}m/s")
+    print(f"Power needed for climb: {fan_1.calc_P_kappa():.2f}W")
+else: #give power, not climb rate
+    print(f"Given power available for climb: {fan_1.P_a:.2f}W")
+    print(f"Axial Rate of Climb achieved: {fan_1.calc_V_c_kappa()[0]:.2f}m/s")
+if fan_1.V_c < 0 and fan_1.V_c/fan_1.v_h >= 2:
+    raise Exception(f'V_c v_h ratio too negative, theory does not apply (current ratio {fan_1.V_c()/fan_1.v_h()}, needs to be greater than -2)')
 
-print(f"Rate of Climb rate: {fan_1.calc_V_c_kappa()}")
+print(f"calc_power_ideal_hover: {fan_1.calc_power_ideal_hover():.2f}")
 
-print(f"calc_power_ideal_hover: {fan_1.calc_power_ideal_hover()}")
-
-print(f"calc_disc_loading: {fan_1.calc_disc_loading()}")
-
-print(f"calc_pidd: {fan_1.calc_p_idd()}")
-
+print(f"calc_disc_loading: {fan_1.calc_disc_loading()[0]:.2f}")
 
 vel = np.arange(0,100,0.01)
 power = []
 for i in vel :
-    #assuming Cd is constant
     fan_2 = ducted_fan_calc.Ducted_Fan_2(mass=float(718/4), Cd0=0.05, V=i, related_fan=fan_1, radius = 0.625)
     power.append(fan_2.calc_p_idf())
-#plot graph 
 power = np.array(power)/int(1000)
 
 # 1. Prepare your data
 # Power lines
-grey = (0.2,0.2,0.2)
 plt.plot(vel, power.T[0], "-", label="Total")  # You can use other types like plt.scatter, plt.bar, etc.
 plt.plot(vel, power.T[1], "--",label="Induced")
 plt.plot(vel, power.T[2], "--",label="parasitic")
@@ -56,7 +55,25 @@ plt.grid(True)                     # Add a grid (optional)
 
 # 4. Display the plot
 plt.show()
+plt.clf
+max_rate = 60
+vertical_rate = np.arange(-max_rate,max_rate, max_rate/250)
+vertical_rate = np.sort(np.append(vertical_rate, 0))
+vertical_power = []
+for i in vertical_rate :
+    fan_1 = ducted_fan_calc.Ducted_Fan_1(mass=float(718/4), V_c=i)
+    vertical_power.append(fan_1.calc_P_kappa())
+vertical_power = np.array(vertical_power)/int(1000)
 
+plt.plot(vertical_rate, vertical_power, "-", label="Power Required")
+plt.plot(0, vertical_power[list(vertical_rate).index(0)], '.', label=f'Hover Power P = {vertical_power[list(vertical_rate).index(0)]:.2f}kW')
+plt.plot((fan_1.v_h*-2, fan_1.v_h*-2), (min(vertical_power),max(vertical_power)), "--", label=f"V_c/v_h < -2\nV_c = {fan_1.v_h*-2:.2f}m/s")
+plt.title('Power Required for vertical rate')
+plt.xlabel('Vertical Rate [m/s]')
+plt.ylabel('Power Required [kW]')
+plt.legend() 
+plt.grid(True)   
+plt.show()
 
 
 # fan_3 = ducted_fan_calc.Ducted_Fan_3(mtow=float(718/4), gamma=2, related_fan1=fan_1, related_fan2 = fan_2 )
