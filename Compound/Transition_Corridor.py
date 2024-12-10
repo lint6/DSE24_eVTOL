9b0792534acaed0660a84ec3a9cd8b51b5b37d67
lintongxuxu <xulintong3@gmail.com>
2024-12-10 10:20:45 +0100

    minor

--- Compound/Transition_Corridor.py
+++ Compound/Transition_Corridor.py
@@ -1,2 +1,12 @@
-
+# Created by Lintong
 import numpy as np
+
+def func_PossibilityCheck(Con1, Con2, Con3):
+    if Con1 and Con2 and Con3:
+        return True
+    else:
+        return False
+    
+def calc_StallCheck(max_alpha = 15):
+    alpha = airpath 
+
